24c2bf6f31563eb70854c407cf4a15f947f50352
Bryce Hayden <bryce.c.hayden@gmail.com>
2022-09-24 08:23:02 -0500

    feat: first callback challenge

--- javascript/06-callbacks/__example__.js
+++ javascript/06-callbacks/__example__.js
@@ -1,43 +1,50 @@
-
 function syncFunc() {
-  return 'I finished as soon as I was called'
+  return "I finished as soon as I was called";
 }
 
 function asyncFunc() {
   setTimeout(() => {
-    return 'I just barely finished'
+    return "I just barely finished";
   }, 5000);
 }
 
 function getSSNSync(state, outputData) {
-  outputData()
+  outputData();
 }
-function getSSNForState(state, outputData){
+function getSSNForState(state, outputData) {
   setTimeout(function () {
-    outputData()
+    outputData();
   }, state.length * 1000);
 }
 
 function asyncFuncWithCallback(timeToWait, myFancyCallback) {
   setTimeout(function () {
-    myFancyCallback(timeToWait)
+    myFancyCallback(timeToWait);
   }, timeToWait * 1000);
 }
 
 function main() {
-  console.log(asyncFunc())
-  console.log(syncFunc())
-  asyncFuncWithCallback(25,
-    function (howLongDidYouWait) {
-       console.log(`I waited: ${howLongDidYouWait}`)
-    }
-  )
-  getSSNSync('Utah', function () { console.log('Now ------')})
-  asyncFuncWithCallback(8, (howLongDidYouWait) => { console.log(`I should have waited the least: ${howLongDidYouWait}`)})
-  asyncFuncWithCallback(17, (howLongDidYouWait) => { console.log(`I waited (middle): ${howLongDidYouWait}`)})
+  console.log(asyncFunc());
+  console.log(syncFunc());
+  asyncFuncWithCallback(25, function (howLongDidYouWait) {
+    console.log(`I waited: ${howLongDidYouWait}`);
+  });
+  getSSNSync("Utah", function () {
+    console.log("Now ------");
+  });
+  asyncFuncWithCallback(8, (howLongDidYouWait) => {
+    console.log(`I should have waited the least: ${howLongDidYouWait}`);
+  });
+  asyncFuncWithCallback(17, (howLongDidYouWait) => {
+    console.log(`I waited (middle): ${howLongDidYouWait}`);
+  });
 
-  getSSNForState('California', function () { console.log('Finished with California')})
-  getSSNForState('Utah', function () { console.log('Finished with Utah')})
+  getSSNForState("California", function () {
+    console.log("Finished with California");
+  });
+  getSSNForState("Utah", function () {
+    console.log("Finished with Utah");
+  });
 }
 
-main()
+main();
